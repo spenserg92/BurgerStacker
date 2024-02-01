0f31fa59fde9bd7ae283a97d446381c2a9665563
spenserg92 <spenserg92@gmail.com>
2024-02-01 12:05:22 -0500

    gets burger stacking working

--- src/BurgerPane.jsx
+++ src/BurgerPane.jsx
@@ -1,11 +1,34 @@
-import React, {Component} from 'react'
+// this component will loop over the burgerIngredients received as a prop from the parent component(BurgerStacker)
+// each iteration of the loop will display one clickable component
+// this clickable item will be rendered by another component, called Ingredient
+
+import React, { Component } from 'react'
+import Ingredient from './Ingredient'
 
 export default class BurgerPane extends Component {
-    render(){
-        console.log('the props in Burger Pane', this.props)
-        return(
+    render () {
+        // console.log('the props in BurgerPane', this.props)
+        // I can use destructuring syntax to isolate anything I'm bringing in from props
+        const { ingredients } = this.props
+        // console.log('the props in ing list', ingredients)
+
+        let burgerBits = ingredients.map((ing, i) => (
+            // this map will immediately return one item per loop iteration
+            <li key={i}>
+                <Ingredient 
+                    ingredient={ing}
+                    clickFunc={() => {console.log('clickFunc')}}
+                    itemKey={i}
+                />
+            </li>
+        ))
+
+        return (
             <section className='pane'>
                 <h3>Burger Stack</h3>
+                <ul>
+                    { burgerBits }
+                </ul>
             </section>
         )
     }
--- src/BurgerStacker.jsx
+++ src/BurgerStacker.jsx
@@ -1,47 +1,72 @@
 import React, { Component } from 'react'
 import IngList from './IngList'
 import BurgerPane from './BurgerPane'
-// class components are al little different that function components
-// they use class style conventions (properties and methods)
-// we have to do things using class style syntaxes (as opposed to function syntax)
-// we'll have a bunch of key:value pairs and methods
-// state in a class component is held within an object
 
+// class components are a little different than function components
+// they use class style conventions (properties, and methods)
+// we have to do things using class style syntaxes(as opposed to function syntax)
+// we'll have a bunch of key:value pairs and methods
+// state in a class component is held within an object.
+
+// to use a class, we have to extend the component class that we bring in from react.
 export default class BurgerStacker extends Component {
-    // if you want to hold state in a class component you use and object
-    // from there, we can update ourt state with a function called setState
-    // another feature
+    // if you want to hold state in a class component, you use an object.
+    // from there, we can update our state with a function called, setState
+    // another feature of class components, is the keyword 'this'
+    // we need to use this when referring to builtin functions, properties, and methods.
     state = {
-        // main list of potential ingredients
-        // both these pieces of state will be passed down to child component
+        // my main list of potential ingredients
+        // both these pieces of state will be passed down to child components
         ingredients: [
-            { name: 'Kaiser Bun', color: 'saddlebrown' },
-            { name: 'Sesame Bun', color: 'sandybrown' },
-            { name: 'Gluten Free Bun', color: 'peru' },
-            { name: 'Lettuce Wrap', color: 'olivedrab' },
-            { name: 'Beef Patty', color: '#3F250B' },
-            { name: 'Soy Patty', color: '#3F250B' },
-            { name: 'Black Bean Patty', color: '#3F250B' },
-            { name: 'Chicken Patty', color: 'burlywood' },
-            { name: 'Lettuce', color: 'lawngreen' },
-            { name: 'Tomato', color: 'tomato' },
-            { name: 'Bacon', color: 'maroon' },
-            { name: 'Onion', color: 'lightyellow' }
+            {name: 'Kaiser Bun', color: 'saddlebrown'},
+            {name: 'Sesame Bun', color: 'sandybrown'},
+            {name: 'Gluten Free Bun', color: 'peru'},
+            {name: 'Lettuce Wrap', color: 'olivedrab'},
+            {name: 'Beef Patty', color: '#3F250B'},
+            {name: 'Soy Patty', color: '#3F250B'},
+            {name: 'Black Bean Patty', color: '#3F250B'},
+            {name: 'Chicken Patty', color: 'burlywood'},
+            {name: 'Lettuce', color: 'lawngreen'},
+            {name: 'Tomato', color: 'tomato'},
+            {name: 'Bacon', color: 'maroon'},
+            {name: 'Onion', color: 'lightyellow'},
+            {name: 'Cheese', color: 'gold'}
         ],
-        // the ingredients on the burger I'm stacking
+        // going to be the ingredients on the burger that I'm stacking
         burgerIngredients: []
     }
 
-    // There is one thing that all class components need to do 
-    // they need to call the render method. Just like all function 
-    // components need to return something
-    render() {
+    // this area will hold our state manipulating functions.
+    // we will pass these functions as props to the correct components to make this app work the way we want it to.
+    addToStack = (e) => {
+        // this method targets the properties of an ingredient
+        // those properties change based on which one is clicked
+        // we use those to build an object that resembles the original ingredient -> {name: 'something', color: 'some color' }
+        // then, we will add that object to the burgerIngredients array
+        // and bc we pass that array to BurgerPane, it will be looped over and render an Ingredient component for each item in the array
+        const ingName = e.target.innerText
+        const ingColor = e.target.style.backgroundColor
+
+        console.log(`clicked ${ingName} and it is ${ingColor}`)
+        // class components use a special method to update their state
+        // This method is called 'setState'
+        // setState is expecting an object, and within that object, we can refer to an individual piece of state to update
+        this.setState({
+            burgerIngredients: [{ name: ingName, color: ingColor }, ...this.state.burgerIngredients]
+        })
+    }
+
+    // there is one thing that all class components need to do
+    // they need to call the render method. Just like all function components need to return something. 
+    render () {
+        // whatever we return from the render method is the jsx we see on the page
         return (
             <>
                 <h1>Burger Stacker</h1>
                 <div className='panes'>
                     <IngList 
                         ingredients={this.state.ingredients}
+                        add={this.addToStack}
                     />
                     <BurgerPane 
                         ingredients={this.state.burgerIngredients}
